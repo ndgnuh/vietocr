62d883e259b6279f21b07cf38d951b7ab1305dad
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-01-02 00:42:18 +0700

    add mv3l_s2s to list of pretrained configs

--- vietocr/tool/config.py
+++ vietocr/tool/config.py
@@ -6,12 +6,12 @@ thisdir = path.dirname(__file__)
 
 url_config = {
     'inception': 'inception_s2s.yml',
-    # 'svtr-t': 'svtr-t-seq2seq.yml',
-    # 'mobilenet_v3_large': 'backbones/mobilenet_v3_large.yml',
+    'mobilenet_v3l_seq2seq': 'mv3l_s2s.yml',
     'vgg_transformer': 'vgg-transformer.yml',
+    'vgg_seq2seq': 'vgg-seq2seq.yml',
+    # 'svtr-t': 'svtr-t-seq2seq.yml',
     # 'resnet_transformer': 'resnet_transformer.yml',
     # 'resnet_fpn_transformer': 'resnet_fpn_transformer.yml',
-    'vgg_seq2seq': 'vgg-seq2seq.yml',
     # 'vgg_convseq2seq': 'vgg_convseq2seq.yml',
     # 'vgg_decoderseq2seq': 'vgg_decoderseq2seq.yml',
     # 'base': 'base.yml',
