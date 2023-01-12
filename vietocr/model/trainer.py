52bdfaabc56bc33a785125c02419aaf79d77b342
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-12 16:01:04 +0700

    adversarial training

--- vietocr/model/trainer.py
+++ vietocr/model/trainer.py
@@ -20,6 +20,7 @@ from ..tool.utils import compute_accuracy
 from ..loader.aug import default_augment
 from ..loader.dataloader import build_dataloader
 
+
 def basic_train_step(lite, model, batch, criterion, optimizer):
     model.train()
     optimizer.zero_grad()
@@ -30,23 +31,39 @@ def basic_train_step(lite, model, batch, criterion, optimizer):
     optimizer.step()
     return loss
 
-def adversarial_train_step(lite, model, batch, criterion, optimizer):
-    # Generating gradient on the input images
+
+def adversarial_train_step(lite, model, batch, criterion, optimizer, epsilon=0.3):
     model.train()
+
+    # Generating gradient on the input images
+    # Using requires grad here because backward doesn't work in eval mode
+    for p in model.parameters():
+        p.requires_grad = False
     optimizer.zero_grad()
     images, labels = batch
-    images.require_grads = True
+    images.requires_grad = True
     outputs = model(images, labels)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    data_grad = images.grad
+    for p in model.parameters():
+        p.requires_grad = True
 
     # Generating adversarial examples
-    loss = criterion(outputs, labels)
-    # TODO: 
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_images = images + epsilon * sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_images = torch.clamp(perturbed_images, 0, 1)
 
-    # Training on the adversarial examples
+    # Train on perturbed image
     optimizer.zero_grad()
+    outputs = model(perturbed_images, labels)
+    loss = criterion(outputs, labels)
     lite.backward(loss)
     optimizer.step()
     return loss
+
 
 class Trainer(LightningLite):
     def __init__(self, config):
@@ -59,7 +76,8 @@ class Trainer(LightningLite):
 
         # Checkpoint and saving
         self.name = config['name']
-        self.output_weights_path = path.join(const.weight_dir, self.name + ".pth")
+        self.output_weights_path = path.join(
+            const.weight_dir, self.name + ".pth")
 
         # Scheduling stuffs
         self.total_steps = training_config['total_steps']
@@ -67,7 +85,6 @@ class Trainer(LightningLite):
         if isinstance(self.validate_every, float):
             self.validate_every = int(self.total_steps * self.validate_every)
         self.print_every = self.validate_every // 5
-
 
         # Models
         # Leave the device stuff to lightning
@@ -85,24 +102,23 @@ class Trainer(LightningLite):
             max_lr=training_config['learning_rate']
         )
 
-
         # Dataloaders
         build_dataloader_ = partial(
             build_dataloader,
-            vocab = self.vocab,
-            image_height = config['image_height'],
-            image_min_width = config['image_min_width'],
-            image_max_width = config['image_max_width'],
-            batch_size = training_config.get('batch_size', 1),
-            num_workers = training_config.get('num_workers', 1),
+            vocab=self.vocab,
+            image_height=config['image_height'],
+            image_min_width=config['image_min_width'],
+            image_max_width=config['image_max_width'],
+            batch_size=training_config.get('batch_size', 1),
+            num_workers=training_config.get('num_workers', 1),
         )
         self.train_data = build_dataloader_(
             annotation_path=training_config['train_annotation'],
-            transform = default_augment
+            transform=default_augment
         )
         self.validate_data = build_dataloader_(
             annotation_path=training_config['validate_annotation'],
-            transform = None
+            transform=None
         )
 
     def run(self):
@@ -140,6 +156,13 @@ class Trainer(LightningLite):
                     optimizer=optimizer,
                     criterion=criterion
                 )
+                adversarial_train_step(
+                    self,
+                    model,
+                    batch,
+                    optimizer=optimizer,
+                    criterion=criterion
+                )
             train_loss.append(loss.item())
 
             lr_scheduler.step()
@@ -159,12 +182,13 @@ class Trainer(LightningLite):
                 new_best = best_full_seq.append(metrics['full_seq'])
                 if new_best:
                     torch.save(model.state_dict(), self.output_weights_path)
-                    tqdm.write(f"Model weights saved to {self.output_weights_path}")
+                    tqdm.write(
+                        f"Model weights saved to {self.output_weights_path}")
 
             if step % print_every == 0:
                 mean_train_loss = train_loss.summarize()
                 lr = optimizer.param_groups[0]['lr']
-                
+
                 info = (
                     f"Training: {step}/{self.total_steps}",
                     f"Loss: {mean_train_loss:.3f}",
@@ -174,7 +198,6 @@ class Trainer(LightningLite):
                     f"GPU time: {gpu_time.summarize():.2f}s",
                 )
                 tqdm.write(" - ".join(info))
-
 
     @torch.no_grad()
     def validate(self):
@@ -200,13 +223,14 @@ class Trainer(LightningLite):
             pr_sents = self.vocab.batch_decode(predictions.tolist())
             gt_sents = self.vocab.batch_decode(labels.tolist())
 
-            full_seq.append(compute_accuracy(pr_sents, gt_sents, 'full_sequence'))
+            full_seq.append(compute_accuracy(
+                pr_sents, gt_sents, 'full_sequence'))
             per_char.append(compute_accuracy(pr_sents, gt_sents, 'per_char'))
 
         metrics = dict(
-            val_loss = val_loss.summarize(),
-            full_seq = full_seq.summarize(),
-            per_char = per_char.summarize(),
+            val_loss=val_loss.summarize(),
+            full_seq=full_seq.summarize(),
+            per_char=per_char.summarize(),
         )
         return metrics
 
