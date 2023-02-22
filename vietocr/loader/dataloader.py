fix CTC loss calculation

vietocr/loader/dataloader.py
@@ -156,7 +156,12 @@ class OCRDataset(Dataset):
 
         img_path = os.path.join(self.root_dir, img_path)
 
-        sample = {'img': img, 'word': word, 'img_path': img_path}
+        sample = {
+            'img': img,
+            'word': word,
+            'img_path': img_path,
+            'target_length': len(word)
+        }
 
         return sample
 
@@ -257,6 +262,9 @@ class Collator(object):
 
         image = torch.FloatTensor(img)
         target = torch.LongTensor(tgt_output)
+        target_lengths = torch.tensor(
+            [sample['target_length'] for sample in batch]
+        )
         # rs = {
         #     'img': image,
         #     'tgt_input': torch.LongTensor(tgt_input),
@@ -265,7 +273,7 @@ class Collator(object):
         #     'filenames': filenames
         # }
 
-        return image, target
+        return image, target, target_lengths
 
 
 def get_lbdm_path(annotation_path: str):

vietocr/model/losses.py
@@ -18,7 +18,7 @@ class CTCLoss(nn.Module):
         self.ctc = nn.CTCLoss(*a, **k)
         self.log_softmax = nn.LogSoftmax(dim=-1)
 
-    def forward(self, outputs, targets):
+    def forward(self, outputs, targets, target_lengths):
         # outputs: [batch, time, class]
         # targets: [batch, max_length]
 
@@ -29,7 +29,7 @@ class CTCLoss(nn.Module):
         logits = self.log_softmax(outputs)
 
         # target_lengths: [batch]
-        target_lengths = torch.count_nonzero(targets != self.ctc.blank, dim=1)
+        # target_lengths = torch.count_nonzero(targets != self.ctc.blank, dim=1)
 
         # input_lengths: [batch]
         # use time * batch for now
@@ -50,7 +50,7 @@ class CrossEntropyLoss(nn.Module):
         k.setdefault("ignore_index", vocab.pad_id)
         self.loss = nn.CrossEntropyLoss(*a, **k)
 
-    def forward(self, outputs, targets):
+    def forward(self, outputs, targets, target_lengths):
         # outputs: [batch, time, num_class]
         # targets: [num_class, batch]
 