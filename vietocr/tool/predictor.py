4a0fe82fab2147544b3302b613232227e0bf83db
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-04-13 21:48:49 +0700

    better score function

--- vietocr/tool/predictor.py
+++ vietocr/tool/predictor.py
@@ -1,6 +1,19 @@
 from .translate import build_model, process_image
+import numpy as np
 
 import torch
+
+
+def score_agg(x, good_threshold):
+    a = x.min(axis=-1)
+    b = x.max(axis=-1)
+    r = b - a
+    x = (x - a) / r
+    x = 1 / (1 + np.exp(30 * (good_threshold - x)))
+    x = x * r + a
+    # score = np.exp((np.exp(x)) / np.exp(1)) / np.exp(1)
+    score = x.mean(axis=-1)
+    return score
 
 
 class Predictor:
@@ -12,7 +25,7 @@ class Predictor:
         self.image_min_width = config['image_min_width']
         self.image_max_width = config['image_max_width']
 
-    @torch.no_grad()
+    @ torch.no_grad()
     def __call__(
         self,
         images,
@@ -39,7 +52,7 @@ class Predictor:
         probs = probs.numpy()
         indices = indices.numpy()
         results = self.vocab.batch_decode(indices.tolist())
-        scores = probs.mean(axis=-1).tolist()
+        scores = score_agg(probs, 0.8).tolist()
         if returns_map:
             return results, scores, output
         else:
